235b4c049ef537446aac0dbff6261db48da71a6d
SamOstr <samostrovsky@gmail.com>
2018-01-19 14:21:08 -0500

    things and stuff

--- screen_battle.py
+++ screen_battle.py
@@ -28,15 +28,15 @@ class Screen_Battle (tkinter.Frame):
         #self.button = tkinter.Button(self, text = "Attack", command = self.attack_clicked, fg = "red", bg = "black").grid(row = 0, column = 0)
 
 
-        self.atk1 = tkinter.Button(self, text = self.player1.attack1, command=self.attack1_clicked)
-        self.atk1.grid(row = 1, column = 0)
-        self.atk2 = tkinter.Label(self, text = "")
-        self.atk2.grid(row = 2, column = 1)
+        self.atk1 = tkinter.Button(self, text = self.player1.attack1, command=self.attack_clicked)
+        self.atk1.grid(row = 7, column = 0)
+        self.atk2 = tkinter.Button(self, text = self.player1.attack2, command=self.attack_clicked)
+        self.atk2.grid(row = 8, column = 0)
 
         self.end = tkinter.Label(self, text = "")
         self.end.grid(row = 3, column = 1)
-        tkinter.Label(self, text = "You", font=self.font).grid(row = 5, column = 0)
-        tkinter.Label(self, text = "Computer",font=self.font).grid(row = 5, column = 1)
+        tkinter.Label(self, text = "You", font=self.font).grid(row = 0, column = 0)
+        tkinter.Label(self, text = "Computer",font=self.font).grid(row = 0, column = 1)
         imageLarge = tkinter.PhotoImage(file="images/" + self.player1.large_image)
         w = tkinter.Label(self, image=imageLarge, )
         w.photo = imageLarge
@@ -50,7 +50,7 @@ class Screen_Battle (tkinter.Frame):
         self.enemy = tkinter.Label(self, text = str(self.player2.hit_points) + "/" + str(self.player2_max_hp) + " HP")
         self.enemy.grid(row = 6, column = 1)
         
-    def attack1_clicked(self):
+    def attack_clicked(self):
         ''' This method is called when the user presses the "Attack" button.
             
             This method does the following:
@@ -58,24 +58,40 @@ class Screen_Battle (tkinter.Frame):
             2) Updates the labels on the top right with the result of the attacks.
             3) Determines if there is a victor.
             4) If there is a victor, removes that Attack button and replaces it with an Exit button.     
-        '''        
+        '''
+
+
+
         if self.player1.attack1 == "Back Stab":
             a1 = self.player1.back_stab(self.player2)
-            a2 = self.player2.back_stab(self.player1)
         elif self.player1.attack1 == "Club Smash":
             a1 = self.player1.club_smash(self.player2)
-            a2 = self.player2.club_smash(self.player1)
         elif self.player1.attack1 == "Hammer Swing":
             a1 = self.player1.hammer_swing(self.player2)
-            a2 = self.player2.hammer_swing(self.player1)
         elif self.player1.attack1 == "Stab":
             a1 = self.player1.stab(self.player2)
-            a2 = self.player2.stab(self.player1)
         elif self.player1.attack1 == "Arcane Blast":
             a1 = self.player1.arcane_blast(self.player2)
-            a2 = self.player2.arcane_blast(self.player1)
 
-        #enemy_attack =
+        if self.player1.attack2 == "Bite":
+            a1 = self.player1.bite(self.player2)
+        elif self.player1.attack2 == "Bow and Arrow":
+            a1 = self.player1.bow_and_arrow(self.player2)
+        elif self.player1.attack2 == "Axe Blow":
+            a1 = self.player1.axe_blow(self.player2)
+        elif self.player1.attack2 == "Sword Slash":
+            a1 = self.player1.sword_slash(self.player2)
+        elif self.player1.attack2 == "Fireball":
+            a1 = self.player1.fireball(self.player2)
+
+        enemy_attack = random.randint(0, 2)
+
+        if enemy_attack == 0:
+            a2 = self.player2.attack1(self.player1)
+        elif enemy_attack == 1:
+            a2 = self.player2.attack2(self.player1)
+        elif enemy_attack == 2:
+            a2 = self.player2.attack3(self.player1)
 
 
         if self.player1.hit_points <= 0 and self.player2.hit_points > 0:
